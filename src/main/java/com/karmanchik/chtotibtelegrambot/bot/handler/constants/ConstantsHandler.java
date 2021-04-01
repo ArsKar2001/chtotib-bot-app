96f9ea42964ba8c7d7e0bfd4a3cbabf2e8be6ee8
Арсений Карманов <arseniy.karmanov@mail.ru>
2021-04-01 18:31:12 +0900

    com - 8.3.4

--- src/main/java/com/karmanchik/chtotibtelegrambot/bot/handler/constants/ConstantsHandler.java
+++ src/main/java/com/karmanchik/chtotibtelegrambot/bot/handler/constants/ConstantsHandler.java
@@ -5,7 +5,7 @@ public class ConstantsHandler {
     public static final String ROLE_TEACHER = "Педагог";
     public static final String CANCEL = "Назад";
     public static final String CREATE = "Создать анкету";
-    public static final String MESSAGE_SPLIT = "<b>" + new String(new char[100]).replace('\0', '-') + "</b>";
+    public static final String MESSAGE_SPLIT = "<b>" + new String(new char[60]).replace('\0', '-') + "</b>";
 
     private ConstantsHandler() {}
 }
