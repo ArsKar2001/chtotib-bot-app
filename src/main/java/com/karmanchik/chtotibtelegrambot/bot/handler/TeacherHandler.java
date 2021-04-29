300c1d12e3ba7bacb95b96f0fc10193db0eb074f
Арсений Карманов <arseniy.karmanov@mail.ru>
2021-04-29 16:58:20 +0900

    comm-8.5.2

--- src/main/java/com/karmanchik/chtotibtelegrambot/bot/handler/TeacherHandler.java
+++ src/main/java/com/karmanchik/chtotibtelegrambot/bot/handler/TeacherHandler.java
@@ -96,6 +96,7 @@ public class TeacherHandler extends MainHandler {
         lessons.stream()
                 .map(Lesson::getDay)
                 .distinct()
+                .sorted()
                 .forEach(day -> {
                     String displayName = DayOfWeek.of(day).getDisplayName(TextStyle.FULL, Helper.getLocale());
                     message.append(MESSAGE_SPLIT).append("\n")
